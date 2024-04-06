1d99c93f14176789a8859a764b1fd78f7d54d5d4
Javiafb <jfernandezb02@gmail.com>
2024-04-06 13:27:07 -0500

    portafolio personal

---
+++ assets/js/script.js
@@ -0,0 +1,199 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Nombre de usuario de tu perfil de GitHub
+  const githubUsername = "Javiafb";
+
+  // URL de la API de GitHub para obtener la información del usuario
+  const githubAPIUrl = `https://api.github.com/users/${githubUsername}?callback=processResponse`;
+
+  // Variable para controlar si se ha completado la carga de la imagen
+  let imageLoaded = false;
+
+  // Función para manejar la carga exitosa de la imagen
+  function handleImageLoad() {
+    // Cambiar la variable de control
+    imageLoaded = true;
+    // Ocultar el mensaje de error, si está presente
+    document.getElementById("error-message").style.display = "none";
+  }
+
+  // Función para manejar el tiempo de espera
+  function handleTimeout() {
+    // Si la imagen aún no se ha cargado, mostrar un mensaje de error
+    if (!imageLoaded) {
+      let imagen_erro = document.getElementById("github-avatar");
+      imagen_erro.style.display = "none";
+
+    //  poner imagen por defecto
+
+      //  let imagen_erro =  document.getElementById("github-avatar").src = "error.png";
+       // Cambia "error.png" por la imagen de error que desees mostrar
+      // document.getElementById("error-message").style.display = "block";
+
+    } 
+  }
+
+  // Agregar una función de callback para procesar la respuesta
+  window.processResponse = function (data) {
+    // Obtener la URL de la imagen de perfil del usuario de GitHub
+    const githubAvatarUrl = data.data.avatar_url;
+
+    // Obtener la referencia al elemento de imagen
+    const img = document.getElementById("github-avatar");
+
+    // Cambiar la fuente de la imagen una vez que se haya cargado la imagen de perfil
+    img.onload = handleImageLoad;
+
+    // Establecer la fuente de la imagen de perfil (esto puede activar el evento onload si la imagen ya está en caché)
+    img.src = githubAvatarUrl;
+  };
+
+  // Crear un elemento <script> para realizar la solicitud JSONP
+  const script = document.createElement("script");
+  script.src = githubAPIUrl;
+
+  // Agregar el script al final del documento para que se ejecute
+  document.body.appendChild(script);
+
+  // Configurar un tiempo de espera de 10 segundos (10000 milisegundos)
+  setTimeout(handleTimeout, 10000);
+});
+
+("use strict");
+
+// element toggle function
+const elementToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
+
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () {
+  elementToggleFunc(sidebar);
+});
+
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+// modal variable
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+
+// modal toggle function
+const testimonialsModalFunc = function () {
+  modalContainer.classList.toggle("active");
+  overlay.classList.toggle("active");
+};
+
+// add click event to all modal items
+for (let i = 0; i < testimonialsItem.length; i++) {
+  testimonialsItem[i].addEventListener("click", function () {
+    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector(
+      "[data-testimonials-title]"
+    ).innerHTML;
+    modalText.innerHTML = this.querySelector(
+      "[data-testimonials-text]"
+    ).innerHTML;
+
+    testimonialsModalFunc();
+  });
+}
+
+// add click event to modal close button
+modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+overlay.addEventListener("click", testimonialsModalFunc);
+
+// custom select variables
+const select = document.querySelector("[data-select]");
+const selectItems = document.querySelectorAll("[data-select-item]");
+const selectValue = document.querySelector("[data-selecct-value]");
+const filterBtn = document.querySelectorAll("[data-filter-btn]");
+
+select.addEventListener("click", function () {
+  elementToggleFunc(this);
+});
+
+// add event in all select items
+for (let i = 0; i < selectItems.length; i++) {
+  selectItems[i].addEventListener("click", function () {
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    elementToggleFunc(select);
+    filterFunc(selectedValue);
+  });
+}
+
+// filter variables
+const filterItems = document.querySelectorAll("[data-filter-item]");
+
+const filterFunc = function (selectedValue) {
+  for (let i = 0; i < filterItems.length; i++) {
+    if (selectedValue === "todo") {
+      filterItems[i].classList.add("active");
+    } else if (selectedValue === filterItems[i].dataset.category) {
+      filterItems[i].classList.add("active");
+    } else {
+      filterItems[i].classList.remove("active");
+    }
+  }
+};
+
+// add event in all filter button items for large screen
+let lastClickedBtn = filterBtn[0];
+
+for (let i = 0; i < filterBtn.length; i++) {
+  filterBtn[i].addEventListener("click", function () {
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    filterFunc(selectedValue);
+
+    lastClickedBtn.classList.remove("active");
+    this.classList.add("active");
+    lastClickedBtn = this;
+  });
+}
+
+// contact form variables
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// add event to all form input field
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+    // check form validation
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+  });
+}
+
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function () {
+    for (let i = 0; i < pages.length; i++) {
+      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        pages[i].classList.add("active");
+        navigationLinks[i].classList.add("active");
+        window.scrollTo(0, 0);
+      } else {
+        pages[i].classList.remove("active");
+        navigationLinks[i].classList.remove("active");
+      }
+    }
+  });
+}
